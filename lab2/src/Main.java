f3c1a573a9527b63fe4a372c7e4b376b00728115
Tulenchi <olya-romanova-03@mail.ru>
2023-11-10 01:12:07 +0300

    1

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -29,53 +29,70 @@ public class Main {
             }
 
             try {
-                BufferedReader reader = new BufferedReader(new FileReader(choice));
+                boolean containsCsv = choice.contains(".csv");
+                boolean containsXml = choice.contains(".xml");
 
-                HashMap<String, Integer> HashmapFromFileDuplicate = new HashMap<>();
-                HashMap<String, Integer> HashmapFromFileFloor = new HashMap<>();
+                if (containsCsv) {
+                    BufferedReader reader = new BufferedReader(new FileReader(choice));
 
-                long startTime = System.currentTimeMillis();
-                while ((line = reader.readLine()) != null) {
-                    String[] parts = line.split(splitBy);
-                    String city = parts[0];
-                    String street = parts[1];
-                    String house = parts[2];
-                    String floor = parts[3];
-                    String key = city + ";" + street + ";" + house + ";" + floor;
-                    String keyFloor = city + ";" + floor;
+                    HashMap<String, Integer> HashmapFromFileDuplicate = new HashMap<>();
+                    HashMap<String, Integer> HashmapFromFileFloor = new HashMap<>();
 
-                    if (HashmapFromFileDuplicate.containsKey(key)) {
-                        HashmapFromFileDuplicate.put(key, HashmapFromFileDuplicate.get(key) + 1);
-                    } else {
-                        HashmapFromFileDuplicate.put(key, 1);
+                    long startTime = System.currentTimeMillis();
+                    while ((line = reader.readLine()) != null) {
+                        String[] parts = line.split(splitBy);
+                        String city = parts[0];
+                        String street = parts[1];
+                        String house = parts[2];
+                        String floor = parts[3];
+                        String key = city + ";" + street + ";" + house + ";" + floor;
+                        String keyFloor = city + ";" + floor;
+
+                        if (HashmapFromFileDuplicate.containsKey(key)) {
+                            HashmapFromFileDuplicate.put(key, HashmapFromFileDuplicate.get(key) + 1);
+                        } else {
+                            HashmapFromFileDuplicate.put(key, 1);
+                        }
+
+                        if (HashmapFromFileFloor.containsKey(keyFloor)) {
+                            HashmapFromFileFloor.put(keyFloor, HashmapFromFileFloor.get(keyFloor) + 1);
+                        } else {
+                            HashmapFromFileFloor.put(keyFloor, 1);
+                        }
+
+                    }
+                    long endTime = System.currentTimeMillis();
+
+                    for (Map.Entry<String, Integer> entry : HashmapFromFileDuplicate.entrySet()) {
+                        String kl = entry.getKey();
+                        Integer vl = entry.getValue();
+                        if (vl != 1) {
+                            System.out.println("House: " + kl + " \nNumber of duplicates: " + vl + "\n");
+                        }
                     }
 
-                    if (HashmapFromFileFloor.containsKey(keyFloor)) {
-                        HashmapFromFileFloor.put(keyFloor, HashmapFromFileFloor.get(keyFloor) + 1);
-                    } else {
-                        HashmapFromFileFloor.put(keyFloor, 1);
+                    for (Map.Entry<String, Integer> entry : HashmapFromFileFloor.entrySet()) {
+                        String kl1 = entry.getKey();
+                        Integer vl1 = entry.getValue();
+                        System.out.println("House (city, floor): " + kl1 + " \nNumber of houses with that many floors: " + vl1 + "\n");
+
                     }
 
-                }
-                long endTime = System.currentTimeMillis();
-
-                for (Map.Entry<String, Integer> entry : HashmapFromFileDuplicate.entrySet()) {
-                    String kl = entry.getKey();
-                    Integer vl = entry.getValue();
-                    if (vl != 1) {
-                        System.out.println("House: " + kl + " \nNumber of duplicates: " + vl + "\n");
-                    }
+                    long time = endTime - startTime;
+                    System.out.println("Time in sec: " + (time * 0.001));
                 }
 
-                for (Map.Entry<String, Integer> entry : HashmapFromFileFloor.entrySet()) {
-                    String kl1 = entry.getKey();
-                    Integer vl1 = entry.getValue();
-                    System.out.println("House (city, floor): " + kl1 + " \nNumber of houses with that many floors: " + vl1 + "\n");
+                else if (containsXml)
+                {
+                    DocumentBuilderFactory dbf;
+                    DocumentBuilder        db ;
+                    Document               doc;
 
                 }
 
-                long time = endTime - startTime;
-                System.out.println("Time in sec: " + (time * 0.001));
+                else {
+                    System.out.println("Wrong file format");
+                }
             }catch(FileNotFoundException e)
             {
                 System.out.println("Wrong file name");
